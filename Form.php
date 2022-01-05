f59a69b0ba172bc81a3babe95ddbc38cf2f922b1
Aurelie.Bergey <aurelie.bergey@gmail.com>
2022-01-05 16:47:17 +0100

    ?? NEW: fichier form.php et index.php

---
+++ Form.php
@@ -0,0 +1,38 @@
+<?php
+
+class Form{
+    private $data; //donnée du formaulaire
+
+    public function __construct ($data){
+        $this->data = $data;
+    }
+
+
+        /**
+         * Get the value of data
+         */ 
+        public function getData()
+        {
+                return $this->data;  // data est un dictionnaire, il comprends la clé et la valeur key ->valeur
+        }
+      
+        private function getValue($key) {
+            if(isset($this->data[$key])) {    
+                return $this->data[$key];
+            } else {
+                return NULL;
+            }
+        }
+
+        public function createImput($key,$class){
+            // echo '<label for="'. $key.'">'.$key.'</label>';
+            echo '<input type="text" class="' .$class. '"    name="' .$key. '" value="' .$this->getValue($key) . '">'; 
+        }
+
+        public function createSubmit($text,$class){
+            echo '<button type="submit" class="' .$class. '" >'.$text.'</button>';
+        }
+
+       
+}
+?>
